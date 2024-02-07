9cf2a394ca09604436d7e389d20c381521942959
Kshitij Gupta <kshitij.gupta2301@gmail.com>
2024-02-08 05:23:27 +0530

    Completed homepage, added googleOauth(nextAuth) and created db

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,16 +6,16 @@ import { signIn, signOut, useSession, getProviders} from 'next-auth/react';
 
 const Nav = () => {
 
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() =>{
-    const setProviders = async () =>{
+      const setUpProviders = async () =>{
       const response = await getProviders();
       setProviders(response);
     }
-    setProviders();
+    setUpProviders();
   },[])
 
   return (
@@ -32,7 +32,7 @@ const Nav = () => {
       </Link>
       {/* Desktop Navigation */}
       <div className='sm:flex hidden'>
-        { isUserLoggedIn ? (
+        { session?.user ? (
           <div className='flex gap-3 md:gap-5'>
               <Link href='/create-prompt' className='black_btn'>
                 Create Post
@@ -45,10 +45,10 @@ const Nav = () => {
               </button>
               <Link href='profile'>
                 <Image 
-                  src='/assets/images/logo.svg' 
+                  src={session?.user.image} 
                   width={37}
                   height={37}
-                  className='rounder-full'
+                  className='rounded-full'
                   alt='profile'
                 />
               </Link>
@@ -72,13 +72,13 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
-          {isUserLoggedIn ? (
+          {session?.user ? (
             <div className='flex '>
               <Image 
-                  src='/assets/images/logo.svg' 
+                  src={session?.user.image}
                   width={37}
                   height={37}
-                  className='rounder-full'
+                  className='rounded-full'
                   alt='profile'
                   onClick={() => setToggleDropdown((prev) => !prev)}
                 />
